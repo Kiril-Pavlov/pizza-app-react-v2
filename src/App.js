b8eb99f4ff921eab139c7dfc212379c8c3dae5ce
Kiril-Pavlov <Kire_P@yahoo.com>
2022-05-08 20:12:56 +0200

    new react project. Created the basic paces and components with router.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router';
 import './App.css';
+
+//pages
+import Home from './pages/Home';
+import About from './pages/About/About';
+import CartPage from './pages/CartPage/CartPage';
+import Contact from './pages/Contact/Contact';
+
+//components
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='about' element={<About/>}/>
+        <Route path='cartpage' element={<CartPage/>}/>
+        <Route path='contact' element={<Contact/>}/>
+      </Routes>
+      <Footer/>
     </div>
   );
 }
